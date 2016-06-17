c680f255c77db59c651214b8d44324f746ee50ce
Ross Hendry <rhendry@googlemail.com>
2016-06-17 10:48:40 +0100

    Fix linting issues
    The comments are needed to disable errors for the non-test.

--- app/__tests__/App.test.js
+++ app/__tests__/App.test.js
@@ -1,7 +1,8 @@
 import test from 'tape';
-import App from '../App';
+import App from '../App'; // eslint-disable-line
 
 test('Include root component', (t) => {
+  // eslint-disable-next-line
   t.test('Component was included to include all files for coverage', (t) => {
     t.equal(true, true);
     t.end();
