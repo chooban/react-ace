58ffbbb884648eddcd711dd9d0e8c78928fd8fe2
Ross Hendry <rhendry@googlemail.com>
2016-10-31 15:48:07 +0000

    Added more tests

---
+++ app/api/__tests__/PreviewsWebApi.test.js
@@ -0,0 +1,52 @@
+/* eslint no-shadow: 0 */
+import fetchMock from 'fetch-mock';
+import test from 'tape';
+
+import { 
+  getIssueList, 
+  getIssue,
+  getLatestIssue
+} from '../PreviewsWebApi';
+
+test('Previews API', (t) => {
+  t.test('Getting the issue list', (t) => {
+    const returnedList = '["337","336","335"]';
+    const expectedResult = ['337', '336', '335'];
+
+    fetchMock.get('/api/previews/', returnedList)
+      
+    return getIssueList().then((d) => {
+      t.deepEqual(d, expectedResult);
+      fetchMock.restore();
+    });
+  });
+
+  t.test('Get an issue', (t) => {
+    const returnedData = '{"file":"ecmail337","contents":[{"previewsCode": "337/0001", "title": "Previews Only #339"}]}';
+    const expectedResult = [{
+      previewsCode: "337/0001",
+      title: "Previews Only #339"
+    }];
+
+    fetchMock.get('/api/previews/337', returnedData);
+    return getIssue(337).then((d) => {
+      t.deepEqual(d, expectedResult);
+      fetchMock.restore();
+    });
+  });
+
+  t.test('Get the latest', (t) => {
+    const returnedData = '{"file":"ecmail337","contents":[{"previewsCode": "337/0001", "title": "Previews Only #339"}]}';
+    const expectedResult = [{
+      previewsCode: "337/0001",
+      title: "Previews Only #339"
+    }];
+
+    fetchMock.get('/api/previews/latest', returnedData);
+    return getLatestIssue().then((d) => {
+      t.deepEqual(d, expectedResult);
+      fetchMock.restore();
+    });
+    
+  });
+});
