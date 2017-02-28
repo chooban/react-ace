da33e7956fad0b120e21a744eb674a166e0574fc
Ross Hendry <rhendry@googlemail.com>
2017-02-28 16:56:43 +0000

    Timing issue fixed

--- app/api/ProfilesWebApi.js
+++ app/api/ProfilesWebApi.js
@@ -2,24 +2,23 @@ import { AuthServiceFactory } from '../utils/AuthService';
 
 const AuthService = AuthServiceFactory();
 
-const acceptJsonHeaders = {
-  headers: {
-    Accept: 'application/json',
-    Authorization: `Bearer ${AuthService.getToken()}`
-  },
-  method: 'POST',
-  cache: false
-};
+const parseData = (resp) => JSON.parse(resp);
 
-function checkStatus(resp) {
+const checkStatus = (resp) => {
   if (resp.status === 200) return resp.text();
 
   return Promise.reject(new Error(resp.statusText));
-}
+};
 
 export function updateProfile() {
-  const parseData = (resp) => JSON.parse(resp);
-
+  const acceptJsonHeaders = {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${AuthService.getToken()}`
+    },
+    method: 'POST',
+    cache: false
+  };
   return fetch('/api/profiles/savedsearches', acceptJsonHeaders)
     .then(checkStatus)
     .then(parseData)
