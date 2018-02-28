f7728726a2492ae65dcb1bb71dc35265d23097df
Ross Hendry <rhendry@googlemail.com>
2018-02-28 15:10:48 +0000

    Only include stats block in prod build
    Closes #29.

--- .webpack/production.js
+++ .webpack/production.js
@@ -1,9 +1,15 @@
 const webpack = require('webpack');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = function() {
   return {
     devtool: 'source-map',
     plugins: [
+      new HtmlPlugin({
+        isProd: 'true',
+        filename: 'index.html',
+        template: 'public/index.hbs'
+      }),
       new webpack.optimize.UglifyJsPlugin({
         sourceMap: true,
         compress: {
