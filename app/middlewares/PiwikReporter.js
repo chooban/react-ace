6a87f1d5c58dd0a477f69bdf0321915fb900ca4f
Ross Hendry <rhendry@googlemail.com>
2018-03-12 12:30:02 +0000

    Move to Google Analytics

--- app/middlewares/PiwikReporter.js
+++ app/middlewares/PiwikReporter.js
@@ -7,10 +7,10 @@ export default () => (next) => (action) => {
   if (triggers.includes(action.type)) {
     switch (action.type) {
       case 'EXPORT_ORDER':
-        _paq.push(['trackEvent', 'order', 'export']); //eslint-disable-line
+        ga('send', 'event', 'order', 'export'); // eslint-disable-line
         break;
       case 'SET_USER_PROFILE':
-        _paq.push(['trackEvent', 'user', 'login']); //eslint-disable-line
+        ga('send', 'event', 'user', 'login'); // eslint-disable-line
         break;
       default:
         break;
